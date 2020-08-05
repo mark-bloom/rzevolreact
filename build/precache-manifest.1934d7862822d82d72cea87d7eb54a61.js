e3b0b74d0935d6d321f4f3ac6c857514f46d9e2e
Mark Bloom <markybloom@gmail.com>
2020-08-05 10:23:27 +1000

    build included

---
+++ build/precache-manifest.1934d7862822d82d72cea87d7eb54a61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc9bea957339b3994dd03fdfa11a3c3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcb057e8709ac329a56a",
+    "url": "/static/css/main.a4392227.chunk.css"
+  },
+  {
+    "revision": "e85fb0c212a050670a1f",
+    "url": "/static/js/2.27ab0b01.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.27ab0b01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcb057e8709ac329a56a",
+    "url": "/static/js/main.da9716c5.chunk.js"
+  },
+  {
+    "revision": "fe7b673dbb7dcc94c15e",
+    "url": "/static/js/runtime-main.48d64271.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
