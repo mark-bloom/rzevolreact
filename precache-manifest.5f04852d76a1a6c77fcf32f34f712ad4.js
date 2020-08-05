0945a60a6aaf303b2bc491f4d14e4cdac59277bd
Mark Bloom <markybloom@gmail.com>
2020-08-05 14:29:20 +1000

    Updates

---
+++ precache-manifest.5f04852d76a1a6c77fcf32f34f712ad4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8bc48d690463ca00e552f6bcd052764",
+    "url": "/rzevolreact/index.html"
+  },
+  {
+    "revision": "8e77d4800a18206a2831",
+    "url": "/rzevolreact/static/css/main.a4392227.chunk.css"
+  },
+  {
+    "revision": "e85fb0c212a050670a1f",
+    "url": "/rzevolreact/static/js/2.27ab0b01.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/rzevolreact/static/js/2.27ab0b01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e77d4800a18206a2831",
+    "url": "/rzevolreact/static/js/main.31963e68.chunk.js"
+  },
+  {
+    "revision": "f318ad4f703be50ba725",
+    "url": "/rzevolreact/static/js/runtime-main.25cf71ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rzevolreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
